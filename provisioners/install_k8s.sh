1c5dd4598d654bc614e95bf289d1eddcfbec2561
alexkonkin <alexkonkin@gmail.com>
2019-09-25 18:35:30 +0300

    Update install_k8s.sh

--- provisioners/install_k8s.sh
+++ provisioners/install_k8s.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo apt update &&\
-sudo apt install -y mc ansible &&\
-mkdir /home/vagrant/k8s &&\
-cd /home/vagrant/k8s &&\
-git clone https://github.com/alexkonkin/k8s-ubuntu.git . &&\
-git checkout k8s_v.16_vagrant &&\
-git branch &&\
+sudo apt update
+sudo apt install -y mc ansible
+mkdir /home/vagrant/k8s
+cd /home/vagrant/k8s
+git clone https://github.com/alexkonkin/k8s-ubuntu.git .
+git checkout k8s_v.16_vagrant
+git branch
 ansible-playbook playbook.yml
 

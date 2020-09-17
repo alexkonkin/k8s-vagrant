872d08c6f067f18606ac9d1acf2ca876513bb4ef
alexkonkin <alexkonkin@gmail.com>
2020-09-17 09:57:26 +0300

    Update install_k8s.sh

--- provisioners/install_k8s.sh
+++ provisioners/install_k8s.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y mc ansible
-#mkdir /home/vagrant/k8s
-mkdir /d/vm/k8s/
-cd /d/vm/k8s
+mkdir /home/vagrant/k8s
+#mkdir /d/vm/k8s/
+#cd /d/vm/k8s
+cd /home/vagrant/k8s/
 git clone https://github.com/alexkonkin/k8s-ubuntu.git .
 git checkout k8s_v.16_vagrant
 git branch

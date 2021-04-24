62bcdeea987310eeafe014e245f3ce79effb014c
alexkonkin <alexkonkin@gmail.com>
2021-04-24 15:16:08 +0300

    added possibility to install particular version

--- provisioners/install_k8s.sh
+++ provisioners/install_k8s.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y mc ansible
-mkdir /home/vagrant/k8s
-#mkdir /d/vm/k8s/
-#cd /d/vm/k8s
-cd /home/vagrant/k8s/
+
+k8s_version=$1
+
+mkdir -pv /home/vagrant/k8s
+cd /home/vagrant/k8s
+
 git clone https://github.com/alexkonkin/k8s-ubuntu.git .
 git checkout k8s_v.16_vagrant
-git branch
-ansible-playbook playbook.yml
 
+echo $k8s_version
+ansible-playbook playbook.yml --extra-vars "k8s_version=$k8s_version"

556c67cefd8184a19be3977330e79d8328cdf69f
alexkonkin <alexkonkin@gmail.com>
2021-04-24 15:32:50 +0300

    Update install_k8s.sh

--- provisioners/install_k8s.sh
+++ provisioners/install_k8s.sh
@@ -8,6 +8,8 @@ cd /home/vagrant/k8s
 git clone https://github.com/alexkonkin/k8s-ubuntu.git .
 git checkout k8s_v.16_vagrant
 
-echo $k8s_version
-sudo apt update && apt install ansible -y
+echo "Selected Kubernetes version is : $k8s_version"
+
+sudo apt update &&\
+sudo apt install ansible -y &&\
 ansible-playbook playbook.yml --extra-vars "k8s_version=$k8s_version"
